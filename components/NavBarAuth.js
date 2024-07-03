b22df334dea37d4d827dae700e031b59ce2f0cfe
Melanie Warwick <princessmelanoke@gmail.com>
2024-07-02 21:17:25 -0500

    updated with proptypes

--- components/NavBarAuth.js
+++ components/NavBarAuth.js
@@ -11,7 +11,7 @@ export default function NavBarAuth() {
     <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
       <Container>
         <Link passHref href="/">
-          <Navbar.Brand>NextJS</Navbar.Brand>
+          <Navbar.Brand>NextJS Counter</Navbar.Brand>
         </Link>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
@@ -19,6 +19,9 @@ export default function NavBarAuth() {
             {/* CLOSE NAVBAR ON LINK SELECTION: https://stackoverflow.com/questions/72813635/collapse-on-select-react-bootstrap-navbar-with-nextjs-not-working */}
             <Link passHref href="/">
               <Nav.Link>Home</Nav.Link>
+            </Link>
+            <Link passHref href="/counter">
+              <Nav.Link>Counter</Nav.Link>
             </Link>
             <Button variant="danger" onClick={signOut}>Sign Out</Button>
           </Nav>
--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,28 @@
 import { Button } from 'react-bootstrap'; // TODO: COMMENT IN FOR AUTH
 import { signOut } from '../utils/auth'; // TODO: COMMENT IN FOR AUTH
 import { useAuth } from '../utils/context/authContext'; // TODO: COMMENT IN FOR AUTH
-import { useState } from 'react';
+import Counter from '../components/Counter';
 
 function Home() {
   const { user } = useAuth(); // TODO: COMMENT IN FOR AUTH
-  const [value, setValue] = useState(0);
-
-  const handleClick = () => {
-    setValue((prevState) => prevState + 1);
-  };
-
+  const counters = [{ title: 'Counter 1' }, { title: 'Counter 2' }, { title: 'Counter 3' }];
+  // const user = { displayName: 'Dr. T' }; // TODO: COMMENT OUT FOR AUTH
   return (
-    <>
-      <h1>Counter Title</h1>
-      <h2>{value}</h2>
-      <Button type="button" onClick={handleClick}>Increment</Button>
-      <Button type="button" onClick={() => setValue((prevState) => prevState - 1)}>Decrement</Button>
-      <Button type="button" onClick={() => setValue(0)}>Reset</Button>
-    </>
+    <div
+      className="text-center d-flex flex-column justify-content-center align-content-center"
+      style={{
+        height: '90vh',
+        padding: '30px',
+        maxWidth: '400px',
+        margin: '0 auto',
+      }}
+    >
+      <h1>Hello {user.displayName}! </h1>
+
+      {counters.map((counter) => (
+        <Counter key={counter.title} title={counter.title} />
+      ))}
+    </div>
   );
 }
 
